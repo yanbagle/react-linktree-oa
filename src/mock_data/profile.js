af525840432951acb34732740038699390ee447b
yan zhang <circusburger63@yahoo.com>
2024-09-16 13:54:10 -0700

    creating mock data

---
+++ src/mock_data/profile.js
@@ -0,0 +1,23 @@
+const profile = {
+  username: "Name",
+  pfp: "",
+  bio: "",
+  socials: [{ img: "", url: "" }],
+  groupLinks: [
+    {
+      title: "",
+      links: [
+        {
+          title: "",
+          img: "",
+        },
+        {
+          title: "",
+          img: "",
+        },
+      ],
+    },
+  ],
+};
+
+export default profile;
